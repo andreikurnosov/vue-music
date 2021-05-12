248d24e2ee9e27e5db30130de9ee3af6fde28877
andreikurnosov <nixdie@yandex.ru>
2021-05-12 10:11:55 +0700

    rendering multiple error msg

--- src/components/Auth.vue
+++ src/components/Auth.vue
@@ -125,14 +125,16 @@
             <!-- Password -->
             <div class="mb-3">
               <label class="inline-block mb-2">Password</label>
-              <vee-field
-                name="password"
-                type="password"
-                class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition
+              <vee-field name="password" :bails="false" v-slot="{ field, errors }">
+                <input
+                  type="password"
+                  class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition
                   duration-500 focus:outline-none focus:border-black rounded"
-                placeholder="Password"
-              />
-              <error-message class="text-red-600" name="password" />
+                  placeholder="Password"
+                  v-bind="field"
+                />
+                <div class="text-red-600" v-for="error in errors" :key="error">{{ error }}</div>
+              </vee-field>
             </div>
             <!-- Confirm Password -->
             <div class="mb-3">
