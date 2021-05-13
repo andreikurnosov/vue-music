46b0b41304cfac54025c8a4abaaa58efc158903a
andreikurnosov <nixdie@yandex.ru>
2021-05-13 11:35:43 +0700

    router links active class

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,7 +3,12 @@
   <header id="header" class="bg-gray-700">
     <nav class="container mx-auto flex justify-start items-center py-5 px-4">
       <!-- App Name -->
-      <router-link class="text-white font-bold uppercase text-2xl mr-4" to="/">Music</router-link>
+      <router-link
+        class="text-white font-bold uppercase text-2xl mr-4"
+        to="/"
+        exact-active-class="no-active"
+        >Music</router-link
+      >
       <div class="flex flex-grow items-center">
         <!-- Primary Navigation -->
         <ul class="flex flex-row mt-1">
@@ -26,6 +31,7 @@
           </template>
         </ul>
       </div>
+      i
     </nav>
   </header>
 </template>
